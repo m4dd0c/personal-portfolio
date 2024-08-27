a087ddfb4a64acaa62bab827e2fb57432307370d
Manish Suthar <manishsuthar078@gmail.com>
2024-08-27 19:12:21 +0530

    projects updated

--- constant/work.data.ts
+++ constant/work.data.ts
@@ -2,6 +2,12 @@ import { WorkDataInterface } from "./types";
 
 export const work: WorkDataInterface[] = [
   {
+    id: "0",
+    link: "https://dev-flood.vercel.app/",
+    title: "DevFlood",
+    desc: "A Stackoverflow clone",
+  },
+  {
     id: "1",
     link: "https://swaaad.netlify.app",
     title: "Swaaad",
@@ -9,20 +15,38 @@ export const work: WorkDataInterface[] = [
   },
   {
     id: "2",
-    link: "https://swaaad.netlify.app",
+    link: "https://studyunknown.netlify.app",
     title: "StudyUnknown",
     desc: "An innovative virtual teaching platform.",
   },
   {
     id: "3",
-    link: "https://swaaad.netlify.app",
+    link: "https://todo-m4dd0x.vercel.app/",
     title: "DoIT",
     desc: "An intuitive to-do application.",
   },
   {
     id: "4",
-    link: "https://swaaad.netlify.app",
-    title: "E-Dukaan",
-    desc: "An e-commerce store.",
+    link: "https://github.com/m4dd0c/skull-movies",
+    title: "Skull-Movies (Mobile App)",
+    desc: "Discover movies and shows with ratings, cast info, and actor filmographies.",
+  },
+  {
+    id: "5",
+    link: "https://github.com/m4dd0c/skull-movies",
+    title: "Todo (Mobile App)",
+    desc: "An auth based todo-application with persistent data.",
+  },
+  {
+    id: "6",
+    link: "https://m4dd0x.netlify.app",
+    title: "kanekiXm4dd0x",
+    desc: "A Personal portfolio with 'tokyo ghoul' scheme.",
+  },
+  {
+    id: "7",
+    link: "https://tictactoe-m4dd0x.netlify.app/",
+    title: "TicTacToe",
+    desc: "A nonstop tic-tac-toe game with scoreboard.",
   },
 ];
