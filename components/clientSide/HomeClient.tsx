530eed14adb09c586b09725b5a7aa6d1763fa810
Manish Suthar <manishsuthar078@gmail.com>
2024-11-18 18:35:07 +0530

    refector: line-break r/n/ used(crlf)

--- components/clientSide/HomeClient.tsx
+++ components/clientSide/HomeClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { isDesktop } from "@/utils/isDesktop";
+import { isDesktop } from "../../utils/isDesktop";
 
 const HomeClient = () => {
   const [isDesktopClient, setIsDesktopClient] = useState(true);
