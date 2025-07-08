7ead6d78ca7f8075e8c5ea2e3ee25d022ed9bb3e
Manish Suthar <manishsuthar078@gmail.com>
2025-07-08 17:14:27 +0530

    feat: migration created

---
+++ prisma/migrations/20250708112436_project_fields_made_optional/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "repo_url" VARCHAR(255),
+ALTER COLUMN "subtitle" DROP NOT NULL,
+ALTER COLUMN "description" DROP NOT NULL,
+ALTER COLUMN "url" DROP NOT NULL;
