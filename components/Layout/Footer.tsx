47716b4ffd316145e39537829669474cf7d8d6b1
m4dd0c <manishsuthar078@gmail.com>
2025-11-12 09:44:54 +0530

    feat: footer component added

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,40 @@
+import { social_media } from "@/lib/constants";
+import HrText from "../ui/HrText";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="bg-white dark:bg-gray-950 pb-5">
+      <HrText>
+        <div className="flex justify-between align-center mb-4 mt-2">
+          <div className="flex justify-start align-center space-x-2"></div>
+          <div>
+            <small className="uppercase leading-loose font-mono">
+              All Rights Reserved © m4dd0c 2025
+            </small>
+          </div>
+        </div>
+      </HrText>
+      <div className="flex flex-col justify-center align-center space-x-3">
+        <div className="flex justify-center align-center space-x-3 mb-2 mt-2">
+          {social_media.map((item) => (
+            <Link
+              href={item.link}
+              key={item.label}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-50/60 hover:text-gray-50 transition mt-1"
+            >
+              <item.icon size={20} />
+            </Link>
+          ))}
+        </div>
+        <small className="uppercase leading-loose font-mono text-center text-gray-50/50">
+          Feel Free to Get in touch
+        </small>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
