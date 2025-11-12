97d7aceb6d807861190ef61df4d3a7cce1c282f2
m4dd0c <manishsuthar078@gmail.com>
2025-11-12 09:46:57 +0530

    feat: oss contri section built

---
+++ components/Sections/OSSContri.tsx
@@ -0,0 +1,23 @@
+import HrText from "../ui/HrText";
+import { github_prs } from "@/lib/constants";
+import { PrCard } from "../ui/PrCard";
+
+const OSSContri = () => {
+  return (
+    <>
+      <HrText>
+        <h2 className="max-w-lg px-2 text-[2.5rem]/10 font-medium tracking-tighter text-balance max-sm:px-4 2xl:mt-0">
+          Top Merged PRs
+        </h2>
+      </HrText>
+      <br />
+      {github_prs.map((pr: PR) => (
+        <HrText key={pr.pr.number}>
+          <PrCard pr={pr} />
+        </HrText>
+      ))}
+    </>
+  );
+};
+
+export default OSSContri;
